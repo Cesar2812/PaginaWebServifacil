75344883998806e6b3aa9458b85b59794f1d821e
Cesar Cerda <cesarcesar2812@outlook.com>
2025-07-20 00:03:44 -0600

    Contratacion

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,47 @@
-/*!
-* Start Bootstrap - Blog Post v5.0.9 (https://startbootstrap.com/template/blog-post)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+///ABRIR MODAL PARA VIDEO EN LA VISTA DE SERVICIOS
+///SERVICIO DE ELECTRICIDAD
+function abrirModal() {
+  $("#formModal").modal("show");
+    $("#mensajeError").hide();
+
+    //cargando la fecha de hoy
+    $("#txtFecha")
+      .datepicker({ dateFormat: "dd/mm/yy" })
+      .datepicker("setDate", new Date());
+
+}
+
+
+function mostrarSelectorHora() {
+  $("#selectorHora").show();
+}
+
+function guardarHora() {
+  const h = $("#rangeHora").val().padStart(2, "0");
+  const m = $("#rangeMinuto").val().padStart(2, "0");
+
+  $("#inputHora").val(`${h}:${m}`);
+  $("#selectorHora").hide();
+}
+
+// Actualizar visualmente los valores de los sliders
+$(function () {
+  $("#rangeHora").on("input", function () {
+    $("#hora-val").text(this.value);
+  });
+
+  $("#rangeMinuto").on("input", function () {
+    $("#minuto-val").text(this.value);
+  });
+
+  $("#rangeSegundo").on("input", function () {
+    $("#segundo-val").text(this.value);
+  });
+
+  // Ocultar si se hace clic fuera del selector
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest("#selectorHora, #inputHora").length) {
+      $("#selectorHora").hide();
+    }
+  });
+});
